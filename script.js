e4aa86e767291e3fc3696c447ccb22bfb9eb0b33
Alexis76430 <alexishequet@yahoo.fr>
2023-12-05 09:29:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".home-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    // navigation: {
+    //     nextE1: "swiper-button-next",
+    //     prevE1: "swiper-button-prev",
+
+    // },
+});
+
+
+let accordions = document.querySelectorAll('.faqs .row .content .box');
+
+accordions.forEach(acco =>{
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => {subAcco.classList.remove('active')});
+        acco.classList.add('active');
+    }
+})
